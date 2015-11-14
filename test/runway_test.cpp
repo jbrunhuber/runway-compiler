447230908fe242d11b44d52119e55328c7dbfd97
jbrunhuber <joshua.brunhuber@gmail.com>
2015-11-14 19:40:45 +0100

    refactoring added double assignment test

--- test/runway_test.cpp
+++ test/runway_test.cpp
@@ -9,7 +9,6 @@
 #include <gtest/gtest.h>
 #include <parser/Parser.hpp>
 
-
 /**
  *
  */
@@ -19,6 +18,9 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
+/**
+ * Tests the allocation of a double
+ */
 TEST(CODEGEN, DECLARATION_DOUBLE) {
 
   const char *expected = "; ModuleID = 'runway'\n"
@@ -28,26 +30,74 @@ TEST(CODEGEN, DECLARATION_DOUBLE) {
       "  %d = alloca double\n"
       "}";
 
-  VariableDeclarationStatement *variableDeclarationStatement = new VariableDeclarationStatement;
+  VariableDeclarationStatement *variable_declaration_statement = new VariableDeclarationStatement;
 
   //type
   PrimaryExpression *type_primary_expression = new PrimaryExpression;
   type_primary_expression->type = ExpressionType::DOUBLE;
 
   //identifier
-  IdentifierPrimaryExpression *identifierPrimaryExpression = new IdentifierPrimaryExpression;
-  identifierPrimaryExpression->type = ExpressionType::IDENTIFIER;
-  identifierPrimaryExpression->string_value = "d";
+  IdentifierPrimaryExpression *identifier_primary_expression = new IdentifierPrimaryExpression;
+  identifier_primary_expression->type = ExpressionType::IDENTIFIER;
+  identifier_primary_expression->string_value = "d";
 
   //build variableDeclarationStatement
-  variableDeclarationStatement->type = type_primary_expression;
-  variableDeclarationStatement->identifier = identifierPrimaryExpression;
-  variableDeclarationStatement->expression_to_assign = nullptr;
+  variable_declaration_statement->type = type_primary_expression;
+  variable_declaration_statement->identifier = identifier_primary_expression;
+  variable_declaration_statement->expression_to_assign = nullptr;
 
   Generator *generator = new Generator;
   generator->construct();
 
-  variableDeclarationStatement->emit(generator);
+  variable_declaration_statement->emit(generator);
+
+  EXPECT_TRUE(strcmp(expected, generator->getIR().c_str()));
+}
+
+/**
+ * Allocates a double and assign a test value
+ */
+TEST(CODEGEN, ASSIGNMENT_DOUBLE) {
+
+  const char *expected = "; ModuleID = 'runway'\n"
+      "\n"
+      "define void @main() {\n"
+      "entrypoint:\n"
+      "  %d = alloca double\n"
+      "  store double 1.330000e+01, double* %d\n"
+      "}";
+
+  VariableDeclarationStatement *variable_declaration_statement = new VariableDeclarationStatement;
+
+  //type
+  PrimaryExpression *type_primary_expression = new PrimaryExpression;
+  type_primary_expression->type = ExpressionType::DOUBLE;
+
+  //identifier
+  IdentifierPrimaryExpression *identifier_primary_expression = new IdentifierPrimaryExpression;
+  identifier_primary_expression->type = ExpressionType::IDENTIFIER;
+  identifier_primary_expression->string_value = "d";
+
+  //double assignment value
+  PrimaryExpression *assignment_value = new PrimaryExpression;
+  assignment_value->type = ExpressionType::DOUBLE;
+  assignment_value->double_value = 13.3;
+
+  //expression node
+  AssignmentExpression *assignment_expr = new AssignmentExpression;
+  assignment_expr->type = ExpressionType::DOUBLE;
+  assignment_expr->identifier = identifier_primary_expression;
+  assignment_expr->expression_to_assign = assignment_value;
+
+  //build variableDeclarationStatement
+  variable_declaration_statement->type = type_primary_expression;
+  variable_declaration_statement->identifier = identifier_primary_expression;
+  variable_declaration_statement->expression_to_assign = assignment_expr;
+
+  Generator *generator = new Generator;
+  generator->construct();
+
+  variable_declaration_statement->emit(generator);
 
   EXPECT_TRUE(strcmp(expected, generator->getIR().c_str()));
 }
