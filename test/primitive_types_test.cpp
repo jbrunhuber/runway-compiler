89075ee8c98f3ce873e5024334051613cedec63f
jbrunhuber <joshua.brunhuber@gmail.com>
2015-11-15 18:51:56 +0100

    added float tests

--- test/primitive_types_test.cpp
+++ test/primitive_types_test.cpp
@@ -103,6 +103,90 @@ TEST(CODEGEN, ASSIGNMENT_DOUBLE) {
 }
 
 /**
+ * Tests the allocation of a double
+ */
+TEST(CODEGEN, DECLARATION_FLOAT) {
+
+  std::string expected = "; ModuleID = 'runway'\n"
+      "\n"
+      "define void @main() {\n"
+      "entrypoint:\n"
+      "  %f = alloca float\n"
+      "}\n";
+
+  VariableDeclarationStatement *variable_declaration_statement = new VariableDeclarationStatement;
+
+  //type
+  PrimaryExpression *type_primary_expression = new PrimaryExpression;
+  type_primary_expression->type = ExpressionType::FLOAT;
+
+  //identifier
+  IdentifierPrimaryExpression *identifier_primary_expression = new IdentifierPrimaryExpression;
+  identifier_primary_expression->type = ExpressionType::IDENTIFIER;
+  identifier_primary_expression->string_value = "f";
+
+  //build variableDeclarationStatement
+  variable_declaration_statement->type = type_primary_expression;
+  variable_declaration_statement->identifier = identifier_primary_expression;
+  variable_declaration_statement->expression_to_assign = nullptr;
+
+  Generator *generator = new Generator;
+  generator->construct();
+
+  variable_declaration_statement->emit(generator);
+
+  EXPECT_EQ(expected, generator->getIR().c_str());
+}
+
+/**
+ * Allocates a double and assign a test value
+ */
+TEST(CODEGEN, ASSIGNMENT_FLOAT) {
+
+  std::string expected = "; ModuleID = 'runway'\n"
+      "\n"
+      "define void @main() {\n"
+      "entrypoint:\n"
+      "  %f = alloca float\n"
+      "  store float 0x402A9999A0000000, float* %f\n"
+      "}\n";
+
+  VariableDeclarationStatement *variable_declaration_statement = new VariableDeclarationStatement;
+
+  //type
+  PrimaryExpression *type_primary_expression = new PrimaryExpression;
+  type_primary_expression->type = ExpressionType::FLOAT;
+
+  //identifier
+  IdentifierPrimaryExpression *identifier_primary_expression = new IdentifierPrimaryExpression;
+  identifier_primary_expression->type = ExpressionType::IDENTIFIER;
+  identifier_primary_expression->string_value = "f";
+
+  //double assignment value
+  PrimaryExpression *assignment_value = new PrimaryExpression;
+  assignment_value->type = ExpressionType::FLOAT;
+  assignment_value->double_value = 13.3;
+
+  //expression node
+  AssignmentExpression *assignment_expr = new AssignmentExpression;
+  assignment_expr->type = ExpressionType::FLOAT;
+  assignment_expr->identifier = identifier_primary_expression;
+  assignment_expr->expression_to_assign = assignment_value;
+
+  //build variableDeclarationStatement
+  variable_declaration_statement->type = type_primary_expression;
+  variable_declaration_statement->identifier = identifier_primary_expression;
+  variable_declaration_statement->expression_to_assign = assignment_expr;
+
+  Generator *generator = new Generator;
+  generator->construct();
+
+  variable_declaration_statement->emit(generator);
+
+  EXPECT_EQ(expected, generator->getIR().c_str());
+}
+
+/**
  * Allocates a double and assign a test value
  */
 TEST(CODEGEN, ASSIGNMENT_INT_TO_DOUBLE) {
