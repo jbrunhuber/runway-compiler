5397a00295813366f586b9ab83b221d9cc534f83
jbrunhuber <joshua.brunhuber@gmail.com>
2015-11-17 19:31:34 +0100

    additive fp type unittest

--- test/additive_codegen_test.cpp
+++ test/additive_codegen_test.cpp
@@ -33,6 +33,64 @@ TEST(CODEGEN, ADDITIVE_INT) {
   Generator *generator = new Generator;
   generator->construct();
 
-  llvm::ConstantInt *llvm_int = (llvm::ConstantInt*)node->emit(generator);
+  llvm::ConstantInt *llvm_int = (
+      llvm::ConstantInt *) node->emit(generator);
   EXPECT_EQ(llvm_int->getZExtValue(), (test_operand_1 + test_operand_2));
+}
+
+/**
+ *
+ */
+TEST(CODEGEN, ADDITIVE_FLOAT) {
+
+  std::string expected = "; ModuleID = 'runway'\n"
+      "\n"
+      "define void @main() {\n"
+      "entrypoint:\n"
+      "  %test_add = alloca double\n"
+      "  store double 0x4026333333333334, double* %test_add\n"
+      "}\n";
+
+  double test_operand_1 = 4.7;
+  double test_operand_2 = 6.4;
+
+  ExpressionType expr_type = ExpressionType::DOUBLE;
+
+  AdditiveExpression *node = new AdditiveExpression;
+
+  PrimaryExpression *number1 = new PrimaryExpression;
+  number1->type = expr_type;
+  number1->double_value = test_operand_1;
+
+  PrimaryExpression *number2 = new PrimaryExpression;
+  number2->type = expr_type;
+  number2->double_value = test_operand_2;
+
+  node->lhs_multiplicative_expression = number1;
+  node->rhs_additive_expression = number2;
+  node->additive_operator = Operator::SUM;
+
+  VariableDeclarationStatement *decl = new VariableDeclarationStatement;
+  PrimaryExpression *type = new PrimaryExpression;
+  type->type = expr_type;
+  decl->type = type;
+
+  IdentifierPrimaryExpression *identifierPrimaryExpression = new IdentifierPrimaryExpression;
+  identifierPrimaryExpression->type = expr_type;
+  identifierPrimaryExpression->string_value = "test_add";
+  decl->identifier = identifierPrimaryExpression;
+
+  AssignmentExpression *assignment_expr = new AssignmentExpression;
+  assignment_expr->type = expr_type;
+  assignment_expr->identifier = identifierPrimaryExpression;
+  assignment_expr->expression_to_assign = node;
+
+  decl->expression_to_assign = assignment_expr;
+
+  Generator *generator = new Generator;
+  generator->construct();
+
+  decl->emit(generator);
+
+  EXPECT_EQ(expected, generator->getIR());
 }
