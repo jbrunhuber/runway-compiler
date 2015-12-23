19ee17aebd700dac6dd3f7026301b3f9570df7ef
Joshua Brunhuber <joshua.brunhuber@gmail.com>
2015-12-23 11:04:52 +0100

    refactoring

--- src/runway/codgen/phi_generator.cpp
+++ src/runway/codgen/phi_generator.cpp
@@ -1,28 +1,26 @@
 //
-// phi_generator.cpp
-// Code generation implementation
-//
 // Created by Joshua Brunhuber on 21.12.2015
-// phi_generator.hpp
-// code generation visitor
 //
-// Created by Joshua Brunhuber on 22.12.2015
-// Copyright (c) 2015 Joshua Brunhuber. All rights reserved.
+// phi_generator.cpp
+// code generation visitor with phi node functionality
 //
 
 #include <codegen/phi_generator.hpp>
-#include <parser/Nodes.hpp>
 
 llvm::Value *phi_generator::emitAssignmentExpression(AssignmentExpression *assignment_expr) {
 
-  llvm::Value *assignment = doAssignment(assignment_expr);;
+  //emit the assignment instructions for the IR
+  llvm::Value *assignment = doAssignment(assignment_expr);
 
+  //determine symbols name as identifier for the phi table
   std::string identifier = assignment_expr->identifier->string_value;
 
+  //create a new phi entry for the symbol
   phi_entry *phi_e = phi_entries_table[identifier];
-
   phi_e->phi_table[_insert_point] = assignment;
 
+  //write the phi entry in the phi table
+  phi_entries_table[identifier] = phi_e;
 
   return assignment;
 };
